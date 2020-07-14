1da8dfad25e131cbc82b4c5b7148489a557b59ec
Minjire <minjirekelvin@gmail.com>
2020-07-14 10:07:12 +0300

    Directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -38,6 +38,7 @@ import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { LoginComponent } from './login/login.component';
+import { HighlightDirective } from './directives/highlight.directive';
 // import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 
@@ -52,6 +53,7 @@ import { LoginComponent } from './login/login.component';
     HomeComponent,
     ContactComponent,
     LoginComponent,
+    HighlightDirective,
   ],
   imports: [
     BrowserModule,
